2c0cd5d90fdf5c32afe6260f08b08993ac3d426f
markobezic <marko.bezic@gmail.com>
2023-03-08 17:41:12 -0500

    added Ginni quote

--- app.js
+++ app.js
@@ -129,6 +129,10 @@ $(document).ready(() => {
         "'If you can’t fly then run, if you can’t run then walk, if you can’t walk then crawl, but whatever you do you have to keep moving forward.'",
       author: "-- Martin Luther King Jr.",
     },
+    {
+      quote: "'Growth and comfort do not coexist'",
+      author: "-- Ginni Rometty",
+    },
   ];
 
   const updateQuote = () => {
